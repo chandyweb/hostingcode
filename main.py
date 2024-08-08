5ffc43ff1865da409f7c843b1f55f379a889cc54
chandyweb <chandyneat6@gmail.com>
2024-08-08 15:03:35 +0700

    prepare

---
+++ main.py
@@ -0,0 +1,35 @@
+# Ex1: Enter text and display only uppercase letter
+# text = input("Enter your Text: ")
+# upper_text = ""
+# for i in range(len(text)):
+#      if text[i] == text[i].upper():
+#         upper_text += text[i]
+# print(upper_text)
+
+# score = int(input("Enter Your score : "))
+# if score < 50:
+#     print("Bad!")
+# elif score >= 50 and score <= 79:
+#     print("Not Bad!,Not Good!")
+# elif score >= 80 and score <= 100:
+#     print("Execellent!")
+# else:
+#     print("Are You crazy!")
+
+# isfound = False
+# while isfound:
+#     number = int(input("Enter Your Number: "))
+#     if number != 10:
+#         print("Try again!")
+#     else:
+#         print("You won!")
+#         isfound = True
+
+# number_of_colume = int(input("Enter Your Number_1 : "))
+# number_of_line_text = int(input("Enter Your Number_2 : "))
+# text = str(input("My letter : "))
+# result = ""
+# for i in range(number_of_colume):
+#         result = text * number_of_line_text
+#         print(result)
+
